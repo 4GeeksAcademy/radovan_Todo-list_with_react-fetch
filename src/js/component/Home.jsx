ef649c7f62c9213fb69d7460ebdcb7b2be237246
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2024-11-05 03:40:52 +0000

    Listo todo list con Fetch

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+
+
+import React, { useState, useEffect } from "react";
+
 
 const initialState = {
   label: "",
@@ -7,8 +10,11 @@ const initialState = {
 
 
 const Home = () => {
+  const apiUrl = "https://playground.4geeks.com/todo"
   const [task, setTask] = useState(initialState);
   const [lisTasks, setLisTasks] = useState([]);
+  const [userName, setUserName] = useState("radovan");
+
 
   // functions
   function handleChange(event) {
@@ -19,9 +25,8 @@ const Home = () => {
   }
 
   function saveTask(event) {
-    if (event.key == "Enter") {
-      setLisTasks([...lisTasks, task]);
-      setTask(initialState);
+    if (event.key == "Enter" && task.label !== "") {
+      addNewTask(userName, task)
     }
   }
 
@@ -30,6 +35,116 @@ const Home = () => {
     setLisTasks(newlist);
   }
 
+  // CREAR USUARIO
+
+  function createUser(name) {
+    fetch(apiUrl + '/users/' + name, {
+      method: "POST",
+
+
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 200, 300, 400, etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como string
+        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+      })
+      .then(data => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        // Manejo de errores
+        console.log(error);
+      });
+  }
+
+  // RECIBIR USER
+
+  function getData(name) {
+    fetch(apiUrl + '/users/' + name)
+      .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        console.log(resp.status); // El código de estado 200, 300, 400, etc.
+        if (resp.status == 404) {
+          createUser(name)
+        }
+        if (resp.ok) {
+          return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+
+        }
+      })
+      .then(data => {
+        if (data) {
+          setLisTasks(data.todos)
+        }
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        // Manejo de errores
+        console.log(error);
+      });
+  }
+
+  // AÑADIENDO NUEVA TAREA A LA API REST
+
+  function addNewTask(name, newTask) {
+    fetch(apiUrl + '/todos/' + name, {
+      method: "POST",
+      body: JSON.stringify(newTask),
+      headers: {
+        "Content-Type": "application/json"
+      }
+
+
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        if (resp.ok) {
+          return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+
+        }
+      })
+      .then(data => {
+        if (data) {
+          setLisTasks([...lisTasks, data])
+          setTask(initialState);
+        }
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        // Manejo de errores
+        console.log(error);
+      });
+  }
+  useEffect(() => {
+    getData(userName)
+  }, [userName])
+
+  // BORRAR TAREAS DE LA API
+
+  function clearTask(todoId) {
+    fetch(apiUrl + '/todos/' + todoId, {
+      method: "DELETE",
+
+
+    })
+      .then(resp => {
+        console.log(resp.ok); // Será true si la respuesta es exitosa
+        getData("radovan")
+        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+      })
+      .then(data => {
+        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+      })
+      .catch(error => {
+        // Manejo de errores
+        console.log(error);
+      });
+  }
   return (
     <div className="fondo">
       <h1 className="text-center pt-5 text-secondary display-1 newFont">
@@ -74,7 +189,7 @@ const Home = () => {
                     {item.label}
                   </li>
                   <div className="trash bg-light  justify-content-end px-2 py-1">
-                    <span className="" onClick={() => deleteTask(index)} >
+                    <span className="" onClick={() => clearTask(item.id)} >
 
                       <i className="fa-solid fa-trash-can py-2 " id="addon-wrapping"></i>
 
